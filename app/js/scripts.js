26f821f8de8a204e58f4a62d90a866b4943cca88
Sergey Peshkov <zloi.g1rz@gmail.com>
2020-10-06 16:45:39 +0800

    Доделал подвал

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,11 +1,11 @@
-function mf_height_function() {
-    var mf_height = $('footer').innerHeight();
-    $('body').css('padding-bottom', mf_height + 'px');
-}
+// function mf_height_function() {
+//     var mf_height = $('footer').innerHeight();
+//     $('body').css('padding-bottom', mf_height + 'px');
+// }
 
-$(window).on('load resize change', function() {
-    mf_height_function();
-});
+// $(window).on('load resize change', function() {
+//     mf_height_function();
+// });
 
 $(document).ready(function() {
     $('.menu-btn').click(function() {
@@ -21,6 +21,14 @@ $(document).ready(function() {
         }
     });
 
+    $('.faq__question').click(function() {
+        if (!$(this).hasClass('active')) {
+            $(this).addClass('active');
+        } else {
+            $(this).removeClass('active');
+        }
+        $(this).siblings('.faq__answer').slideToggle();
+    });
 
     // $('.using-slider').owlCarousel({
     //     items: 1,
